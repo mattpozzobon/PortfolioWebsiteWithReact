1d9b526751f3bc8fc1cfabb83028b14fe5220838
Matheus <83451650+mattpozzobon@users.noreply.github.com>
2023-05-13 16:08:07 +0100

    changed picture description

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,7 +3,7 @@ import profilePic from '../images/profilepic.png';
 export default function Profile() {
   return (
     <div>
-      <img className="profile_picture" src={profilePic}/>
+      <img className="profile_picture" src={profilePic} alt='Myself'/>
       <div className='profile_text'>
           <h1 className='name'>Matheus</h1>
           <h1 className='name'>Pozzobon</h1>
